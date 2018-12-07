be72fefe4cefadfcae524fd4b2eb1184ed7b705e
Carlos Scheidegger <cscheid@cs.arizona.edu>
2018-12-06 19:23:32 -0700

    now with a rocket web server as the control plane

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate serde_derive;
 extern crate byteorder;
 
 use std::vec::Vec;
@@ -7,10 +12,33 @@ use std::io::prelude::*;
 use std::path::Path;
 use std::error::Error;
 use byteorder::ReadBytesExt;
-use byteorder::{ByteOrder, BigEndian};
+use byteorder::BigEndian;
 use std::io::Cursor;
+use std::thread;
+use std::sync::mpsc::channel;
+use std::sync::mpsc::Sender;
+use rocket::State;
+use std::sync::Mutex;
 
 mod um;
+
+struct CommandSender(Mutex<Sender<um::Command>>);
+
+#[post("/save/<file_name>")]
+fn save(file_name: String, command_sender: State<CommandSender>) -> String
+{
+    let r = command_sender.0.lock().unwrap();
+    r.send(um::Command::SaveState(file_name.clone())).unwrap();
+    format!("Saved to {}", file_name)
+}
+
+#[post("/load/<file_name>")]
+fn load(file_name: String, command_sender: State<CommandSender>) -> String
+{
+    let r = command_sender.0.lock().unwrap();
+    r.send(um::Command::LoadState(file_name.clone())).unwrap();
+    format!("Loaded from {}", file_name)
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -33,15 +61,27 @@ fn main() {
     // };
     // println!("{:?}", buffer.len());
 
-
     // seems inefficient, but shrug
     let mut buffer_u8 = Vec::new();
-    file.read_to_end(&mut buffer_u8);
+    file.read_to_end(&mut buffer_u8).expect("file should be readable");
 
     let mut buffer = vec![0; buffer_u8.len() / 4]; // Vec::new();
     let mut rdr = Cursor::new(buffer_u8);
-    rdr.read_u32_into::<BigEndian>(&mut buffer);
+    rdr.read_u32_into::<BigEndian>(&mut buffer).expect("buffer should be readable");
 
-    let mut machine = um::init(buffer);
-    machine.run();
+    let (command_sender, command_receiver) = channel();
+
+    let machine_thread = thread::spawn(move || {
+        let mut machine = um::init(buffer, command_receiver);
+        machine.run();
+    });
+
+    let _rocket_thread = thread::spawn(move || {
+        rocket::ignite()
+            .mount("/", routes![save, load])
+            .manage(CommandSender(Mutex::new(command_sender)))
+            .launch();
+    });
+                                     
+    machine_thread.join().unwrap();
 }
--- src/um.rs
+++ src/um.rs
@@ -1,139 +1,202 @@
-use std::collections::HashMap;
+extern crate serde_derive;
+extern crate serde;
+extern crate bincode;
+
 use std::vec::Vec;
 use std::io;
 use std::io::Write;
 use std::io::Read;
+use std::fs::File;
+use bincode::serialize_into;
+use bincode::deserialize_from;
+use std::io::BufWriter;
+use std::io::BufReader;
+use std::sync::mpsc::channel;
+use std::thread;
+use std::sync::mpsc::Receiver;
 
 // http://www.boundvariable.org/um-spec.txt
 
-pub struct Machine {
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Command {
+    SaveState(String),
+    LoadState(String)
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MachineState {
     count: usize,
     registers: [u32; 8],
-    arrays: Vec<Vec<u32>>, // HashMap<u32, Vec<u32>>,
+    arrays: Vec<Vec<u32>>,
     array_list: Vec<u32>,
     execution_finger: usize,
     halted: bool
 }
 
-impl Machine {
+pub struct Machine {
+    state: MachineState,
+    command_receiver: Receiver<Command>
+}
 
+impl Machine {
     fn alloc(&mut self, n: u32) -> u32 {
-        if self.array_list.len() > 0 {
-            let ix = self.array_list.pop().unwrap();
-            self.arrays[ix as usize].append(&mut vec![0; n as usize]);
+        if self.state.array_list.len() > 0 {
+            let ix = self.state.array_list.pop().unwrap();
+            self.state.arrays[ix as usize].append(&mut vec![0; n as usize]);
             ix as u32
         } else {
-            let ix = self.arrays.len();
-            self.arrays.push(vec![0; n as usize]);
+            let ix = self.state.arrays.len();
+            self.state.arrays.push(vec![0; n as usize]);
             ix as u32
         }
     }
 
     fn free(&mut self, a: u32) {
-        self.arrays[a as usize].truncate(0);
-        self.array_list.push(a);
+        self.state.arrays[a as usize].truncate(0);
+        self.state.array_list.push(a);
+    }
+    pub fn snapshot(&self, filename: &String) {
+        let mut f = BufWriter::new(File::create(filename).unwrap());
+        serialize_into(&mut f, &self.state).unwrap();
+    }
+    pub fn load_from_snapshot(&mut self, filename: &String) {
+        let f = BufReader::new(File::open(filename).unwrap());
+        self.state = deserialize_from(f).unwrap();
     }
     pub fn run(&mut self) {
-        while !self.halted {
+        while !self.state.halted {
             self.step();
         }
     }
+    pub fn process_commands(&mut self) -> () {
+        let v = self.command_receiver.try_recv();
+        match v {
+            Ok(Command::SaveState(filename)) => {
+                println!("Will save! {}", &filename);
+                self.snapshot(&filename);
+            },
+            Ok(Command::LoadState(filename)) => {
+                println!("Will load! {}", &filename);
+                self.load_from_snapshot(&filename);
+            },
+            Err(_) => {}
+        }
+    }
+
     pub fn step(&mut self) {
-        self.count += 1;
-        let current_platter = self.arrays[0][self.execution_finger];
-        self.execution_finger += 1;
+        self.state.count += 1;
+        let current_platter = self.state.arrays[0][self.state.execution_finger];
+        self.state.execution_finger += 1;
         let current_operator = current_platter >> 28;
         let reg_c = (current_platter & 7) as usize;
         let reg_b = ((current_platter >> 3) & 7) as usize;
         let reg_a = ((current_platter >> 6) & 7) as usize;
         match current_operator {
             0  => {
-                if self.registers[reg_c] != 0 {
-                    self.registers[reg_a] = self.registers[reg_b];
+                if self.state.registers[reg_c] != 0 {
+                    self.state.registers[reg_a] = self.state.registers[reg_b];
                 };
             },
             1  => {
-                let b = self.registers[reg_b] as usize;
-                let c = self.registers[reg_c] as usize;
-                self.registers[reg_a] = self.arrays[b][c];
+                let b = self.state.registers[reg_b] as usize;
+                let c = self.state.registers[reg_c] as usize;
+                self.state.registers[reg_a] = self.state.arrays[b][c];
             },
             2  => {
-                let a = self.registers[reg_a] as usize;
-                let b = self.registers[reg_b] as usize;
-                let c = self.registers[reg_c];
-                self.arrays[a][b] = c;
+                let a = self.state.registers[reg_a] as usize;
+                let b = self.state.registers[reg_b] as usize;
+                let c = self.state.registers[reg_c];
+                self.state.arrays[a][b] = c;
             },
             3  => {
-                self.registers[reg_a] = self.registers[reg_b].wrapping_add(self.registers[reg_c]);
+                self.state.registers[reg_a] = self.state.registers[reg_b].wrapping_add(self.state.registers[reg_c]);
             },
             4  => {
-                self.registers[reg_a] = self.registers[reg_b].wrapping_mul(self.registers[reg_c]);
+                self.state.registers[reg_a] = self.state.registers[reg_b].wrapping_mul(self.state.registers[reg_c]);
             },
             5  => {
-                self.registers[reg_a] = self.registers[reg_b].wrapping_div(self.registers[reg_c]);
+                self.state.registers[reg_a] = self.state.registers[reg_b].wrapping_div(self.state.registers[reg_c]);
             },
             6  => {
-                self.registers[reg_a] = !(self.registers[reg_b] & self.registers[reg_c]);
+                self.state.registers[reg_a] = !(self.state.registers[reg_b] & self.state.registers[reg_c]);
             },
             7  => {
-                self.halted = true;
-                eprintln!("Halted after {:?} instructions", self.count);
+                self.state.halted = true;
+                eprintln!("Halted after {:?} instructions", self.state.count);
             },
             8  => {
-                self.registers[reg_b] = self.alloc(self.registers[reg_c]);
+                self.state.registers[reg_b] = self.alloc(self.state.registers[reg_c]);
             },
             9  => {
-                self.free(self.registers[reg_c]);
+                self.free(self.state.registers[reg_c]);
             },
             10 => {
                 let mut stdout = io::stdout();
                 {
                     let mut handle = stdout.lock();
                     let mut buffer = [0; 1];
-                    buffer[0] = self.registers[reg_c] as u8;
-                    handle.write(&buffer);
+                    buffer[0] = self.state.registers[reg_c] as u8;
+                    handle.write(&buffer).expect("stdout should be writable");
                 }
-                stdout.flush();
-                // println!("{:?}", self.registers[reg_c] as u8);
+                stdout.flush().expect("stdout should be flushable");
+                // println!("{:?}", self.state.registers[reg_c] as u8);
             },
             11 => {
-                let stdin = io::stdin();
-                let mut handle = stdin.lock();
-                let mut buffer = [0; 1];
-                match handle.read(&mut buffer) {
-                    Ok(_) => self.registers[reg_c] = buffer[0].into(),
-                    Err(_) => self.registers[reg_c] = !0
-                };
+                let (io_sender, io_receiver) = channel();
+                let _io_thread = thread::spawn(move || {
+                    let stdin = io::stdin();
+                    let mut handle = stdin.lock();
+                    let mut buffer = [0; 1];
+                    let v = match handle.read(&mut buffer) {
+                        Ok(_) =>  buffer[0].into(),
+                        Err(_) => !0
+                    };
+                    io_sender.send(v).unwrap();
+                });
+                let mut done = false;
+                while !done {
+                    let io_result = io_receiver.try_recv();
+                    match io_result {
+                        Ok(v) => {
+                            self.state.registers[reg_c] = v;
+                            done = true;
+                        },
+                        Err(_) => {}
+                    }
+                    self.process_commands();
+                }
             },
             12 => {
-                let v = self.registers[reg_b];
-                self.execution_finger = self.registers[reg_c] as usize;
-                let b = self.registers[reg_b] as usize;
+                let v = self.state.registers[reg_b];
+                self.state.execution_finger = self.state.registers[reg_c] as usize;
+                let b = self.state.registers[reg_b] as usize;
                 if v != 0 {
-                    self.arrays[0].truncate(0);
-                    let mut a = self.arrays[b].clone();
-                    self.arrays[0].append(&mut a);
+                    self.state.arrays[0].truncate(0);
+                    let mut a = self.state.arrays[b].clone();
+                    self.state.arrays[0].append(&mut a);
                 }
             },
             13 => {
                 let imd_reg = (current_platter >> 25) & 7;
                 let imd_value = current_platter & ((1 << 25) - 1);
-                self.registers[imd_reg as usize] = imd_value;
+                self.state.registers[imd_reg as usize] = imd_value;
             },
             _ => {}
         };
     }
 }
 
-pub fn init(program: Vec<u32>) -> Machine {
-    let mut arrays = vec![program];
-    // arrays.insert(0, program);
+pub fn init(program: Vec<u32>, command_receiver: Receiver<Command>) -> Machine {
+    let arrays = vec![program];
     Machine {
-        count: 0,
-        registers: [0; 8],
-        arrays: arrays,
-        array_list: Vec::new(),
-        execution_finger: 0,
-        halted: false
+        state: MachineState {
+            count: 0,
+            registers: [0; 8],
+            arrays: arrays,
+            array_list: Vec::new(),
+            execution_finger: 0,
+            halted: false
+        },
+        command_receiver: command_receiver
     }
 }
